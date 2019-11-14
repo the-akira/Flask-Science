7f2a5a312adf5a4ef8fa84e39e36771adf121371
the-akira <gabrielfelippe90@gmail.com>
2019-11-14 01:00:10 -0300

    dois pequenos apps adicionados

---
+++ app1/controller.py
@@ -0,0 +1,5 @@
+import model, view
+
+# python controller.py [parâmetro numérico]
+model.r = view.get_input()
+view.present_output(model.r)
---
+++ app1/hw.py
@@ -0,0 +1,5 @@
+# python hw.py [parâmetro numérico]
+import sys, math
+r = float(sys.argv[1])
+s = math.sin(r)
+print('Hello, World! sin(%g)=%g' % (r, s))
